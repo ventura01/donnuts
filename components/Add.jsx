ae0c5d0533f37a6ecc9b9ffac48fbaa34e9e1efa
rafael <rventu@gmail.com>
2022-06-28 15:57:52 -0600

    Cloudinary

--- components/Add.jsx
+++ components/Add.jsx
@@ -4,12 +4,83 @@ import { useRouter } from "next/router";
 import styles from "../styles/Add.module.css";
 
 const Add = ({ setClose }) => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [price, setPrice] = useState(null);
+
+  const handleCreate = async () => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "uploads");
+
+    try {
+      const uploadRes = await axios.post(
+        "http://api.cloudinary.com/v1_1/the-webdev/upload",
+        data
+      );
+      //   console.log(uploadRes.data);
+      const { url } = uploadRes.data;
+      const newProduct = { file, desc, title, price, img: url };
+
+      await axios.post("http://localhost:3000/api/products", newProduct);
+      setClose(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <span className={styles.close} onClick={() => setClose(true)}>
           X
         </span>
+        <h1>Add new product</h1>
+        <div className={styles.item}>
+          <label htmlFor="file" className={styles.label}>
+            Choose an image
+          </label>
+          <input
+            type="file"
+            className={styles.input}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="title" className={styles.label}>
+            Title
+          </label>
+          <input
+            type="text"
+            className={styles.input}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="desc" className={styles.label}>
+            Description
+          </label>
+          <textarea
+            placeholder="Once upon a time..."
+            rows={4}
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="price" className={styles.label}>
+            Price
+          </label>
+          <input
+            placeholder="Price"
+            type="number"
+            className={styles.input}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          Create
+        </button>
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "images.unsplash.com", "assets.vercel.com"],
+    domains: [
+      "localhost",
+      "images.unsplash.com",
+      "assets.vercel.com",
+      "res.cloudinary.com",
+    ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
